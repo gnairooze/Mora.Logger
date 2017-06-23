799ee6e7700fcf2b4bf462a4368a33b8e5cd8ecd
George Nairooze <gnairooze@gmail.com>
2017-06-23 14:31:29 +0200

    add to git

---
+++ LoggerHelper/Utility.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mora.Logger.LoggerHelper
+{
+    public class Utility
+    {
+        public static string ExtractError(Exception ex, ref StringBuilder bld)
+        {
+            if (bld == null)
+            {
+                bld = new StringBuilder();
+            }
+
+            if (ex.Data != null && ex.Data.Count > 0)
+            {
+                foreach (DictionaryEntry item in ex.Data)
+                {
+                    bld.AppendLine(String.Format("Data Key:{0}. Data Value:{1}.", item.Key, item.Value));
+                }
+            }
+
+            bld.AppendLine(String.Format("Message:{0}.", ex.Message));
+            bld.AppendLine(String.Format("StackTrace:{0}.", ex.StackTrace));
+
+            if (ex.InnerException != null)
+            {
+                ExtractError(ex.InnerException, ref bld);
+            }
+
+            return bld.ToString();
+        }
+        public static string GetWho(string className, string methodName, string clientIp)
+        {
+            string name = System.Net.Dns.GetHostName();
+            System.Net.IPAddress[] ips = System.Net.Dns.GetHostAddresses(name);
+
+            List<string> allIPs = new List<string>();
+
+            if (ips != null)
+            {
+                foreach (System.Net.IPAddress ip in ips)
+                {
+                    allIPs.Add(ip.ToString());
+                }
+            }
+
+            System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
+
+            string who = string.Format("Client IP:{0} | HostName:{1} | IPs:{2} | Location:{3} | Assembly:{4} | Class:{5} | Method:{6}", clientIp, name, string.Join(",", allIPs.ToArray()), assembly.Location, assembly.FullName, className, methodName);
+
+            return who;
+        }
+    }
+}
